4002e6bf80c56aedb036d86c427e55e92a34d1b7
Tim <tim.metzger.ch@gmail.com>
2022-04-22 16:01:30 +0200

    fruit

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import pygame, sys
+from pygame.math import Vector2
+
+class FRUIT:
+    def __init__(self):
+        self.x = 5
+        self.y = 5
+        self.pos = Vector2(self.x,self.y) 
+
+    def draw_fruit(self):
+        fruit_rect = pygame.Rect(self.pos.x,self.pos.y,cell_size,cell_number)
+        pygame.draw.rect(screen,(126,166,114),fruit_rect)
 
 pygame.init()
-cell_size = 36
-cell_number = 18
+cell_size = 40
+cell_number = 20
 screen = pygame.display.set_mode((cell_number * cell_size,cell_number * cell_size))
 clock = pygame.time.Clock()
+
+fruit = FRUIT()
 
 while True:
     for event in pygame.event.get():
@@ -12,6 +25,9 @@ while True:
             pygame.quit()
             sys.exit()
 
-screen.fill((175,215,70))
-pygame.display.update()
-clock.tick(60)
+    screen.fill((175,215,70))
+    fruit.draw_fruit()
+    pygame.display.update()
+    clock.tick(60)        
+
+
